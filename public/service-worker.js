72f1eea33941feece06744f5a56840fd200e4473
Josh8903 <thorngren87@gmail.com>
2020-11-16 21:47:48 -0600

    not functional

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
+  "/db.js",
   "/styles.css",
   "https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap",
   "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
